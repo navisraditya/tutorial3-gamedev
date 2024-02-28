306bf224588f229d4cac2d23a11ae06581181eb0
navisraditya <navisraditya@gmail.com>
2024-02-28 20:56:49 +0700

    dash; first try

--- scenes/PlayerMovement.gd
+++ scenes/PlayerMovement.gd
@@ -5,11 +5,17 @@ const GRAVITY = 1200
 
 const UP = Vector2(0, -1)
 const jump = 250
+var max_speed = 2000
+var curr_speed : float = 0
+var sliding : bool = false
+var dash_amount = 1
 var double_jump = 2
 var velocity = Vector2()
 
 func get_input():
+	var input_direction = Input.get_axis("left", "right")
 	velocity.x = 0
+	
 	if Input.is_action_pressed('right'):
 		velocity.x += speed
 	if Input.is_action_pressed('left'):
@@ -17,12 +23,41 @@ func get_input():
 	if Input.is_action_just_pressed("jump") && double_jump > 0:
 		velocity.y = -jump
 		double_jump -= 1
+	
+	if Input.is_action_pressed("slide"):
+		curr_speed -= velocity.normalized().x * 20
+		
+	if !Input.is_action_pressed("slide"):
+		curr_speed =  input_direction * speed
+	if Input.is_action_just_pressed("slide") && dash_amount > 0:
+		curr_speed += input_direction * speed * 2
+		dash_amount -= 1
+		
+	if !Input.is_action_just_pressed("slide"):
+		if !is_on_floor():
+			if(input_direction != 0):
+				curr_speed += input_direction * speed * 0.5
+				curr_speed = min(curr_speed, speed)
+				curr_speed = max(curr_speed, speed)
+			else:
+				curr_speed -= velocity.normalized().x * 10
+		else:
+			if !sliding:
+				curr_speed = input_direction * speed
+	
+	curr_speed = min(curr_speed, max_speed)
+	curr_speed = max(curr_speed, -max_speed)
+	
 	if is_on_floor():
 		double_jump = 1
-
 		
 
 func _physics_process(delta):
 	velocity.y += delta * GRAVITY
 	get_input()
 	velocity = move_and_slide(velocity, UP)
+
+func dash(dash_force):
+	if(curr_speed != 0):
+		curr_speed += (curr_speed / abs(curr_speed)) * dash_force
+		
