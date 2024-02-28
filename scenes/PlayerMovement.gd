0f74c18a8b77d4fe3b4d1ee2fe31b79d16f04925
navisraditya <navisraditya@gmail.com>
2024-02-28 20:41:41 +0700

    double jump

--- scenes/PlayerMovement.gd
+++ scenes/PlayerMovement.gd
@@ -1,10 +1,11 @@
 extends KinematicBody2D
 
 export (int) var speed = 400
-const GRAVITY = 10
+const GRAVITY = 1200
 
 const UP = Vector2(0, -1)
 const jump = 250
+var double_jump = 2
 var velocity = Vector2()
 
 func get_input():
@@ -13,10 +14,15 @@ func get_input():
 		velocity.x += speed
 	if Input.is_action_pressed('left'):
 		velocity.x -= speed
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") && double_jump > 0:
 		velocity.y = -jump
+		double_jump -= 1
+	if is_on_floor():
+		double_jump = 1
+
+		
 
 func _physics_process(delta):
-	velocity.y += GRAVITY
+	velocity.y += delta * GRAVITY
 	get_input()
 	velocity = move_and_slide(velocity, UP)
